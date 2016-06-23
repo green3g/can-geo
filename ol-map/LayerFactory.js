6d6dd045e022c0827035926651121729c02fd60f
roemhildtg <roemhildtg@gmail.com>
2016-06-23 16:38:09 -0500

    Enhanced layer factory

--- ol-map/LayerFactory.js
+++ ol-map/LayerFactory.js
@@ -1,5 +1,3 @@
-
-
 import ol from 'openlayers';
 import can from 'can';
 
@@ -7,7 +5,8 @@ var layerClass = {
   TileWMS: 'Tile',
   Group: 'Group',
   OSM: 'Tile',
-  ImageWMS: 'Image'
+  ImageWMS: 'Image',
+  ImageVector: 'Image'
 };
 
 var currentId = 0;
@@ -15,50 +14,66 @@ var getNextId = function() {
   return currentId++;
 };
 
-export default {
-  getLayer: function(options) {
-    //if user provides the actual layer, skip the layer factory
-    if (options instanceof ol.layer.Base) {
-      return options;
-    }
-    //get the layer class name
-    //different from our short name
-    //For example, layer type TileWMS and ImageWMS both use type 'Tile'
-    var type = layerClass[options.type];
-
-    //the user custom constructor options
-    var userOptions = options.attr('options') ? options.attr('options').attr() : {};
-
-    //the required parameters for the layer
-    var requiredOptions = this[options.type](options);
-
-    //make sure the layer has a unique id
-    if (!userOptions.id) {
-      userOptions.id = 'layer-' + getNextId();
-    }
-
-    //mixin all of our configuration options for the constructor
-    return new ol.layer[type](can.extend(userOptions, requiredOptions));
-  },
-  ImageWMS: function(options) {
-    return {
-      source: new ol.source.ImageWMS(options.sourceOptions.attr())
-    };
-  },
-  TileWMS: function(options) {
-    return {
-      source: new ol.source.TileWMS(options.sourceOptions.attr())
-    };
-  },
-  Group: function(options) {
-    return {
-      layers: new ol.Collection(options.options.layers.reverse().map(this.getLayer.bind(this)))
-    };
-  },
-  OSM: function(options) {
-    var sourceOptions = options.attr('sourceOptions') ? options.attr('sourceOptions').attr() : {};
-    return {
-      source: new ol.source.OSM(sourceOptions)
-    };
+function convertToObject(obj) {
+  if (obj.attr) {
+    return obj.attr();
   }
+  return obj;
+}
+
+export default {
+  getLayer(options) {
+      options = convertToObject(options);
+      //if user provides the actual layer, skip the layer factory
+      if (options instanceof ol.layer.Base) {
+        return options;
+      }
+      //get the layer class name
+      //different from our short name
+      //For example, layer type TileWMS and OSM both use type 'Tile'
+      var type = layerClass[options.type];
+
+      //the user custom constructor options
+      var userOptions = options.options ? options.options : {};
+
+      //the required parameters for the layer
+      var requiredOptions = this[options.type](options);
+
+      //make sure the layer has a unique id
+      if (!userOptions.id) {
+        userOptions.id = 'layer-' + getNextId();
+      }
+
+      //mixin all of our configuration options for the constructor
+      return new ol.layer[type](can.extend(userOptions, requiredOptions));
+    },
+    ImageWMS(options) {
+      return {
+        source: new ol.source.ImageWMS(options.sourceOptions)
+      };
+    },
+    ImageVector(options) {
+      return {
+        source: new ol.source.ImageVector(options.sourceOptions)
+      };
+    },
+    TileWMS(options) {
+      return {
+        source: new ol.source.TileWMS(options.sourceOptions)
+      };
+    },
+    Group(options) {
+      let layers = new List(options.options.layers)
+        .reverse()
+        .map(this.getLayer.bind(this));
+      return {
+        layers: new ol.Collection(layers)
+      };
+    },
+    OSM(options) {
+      var sourceOptions = options.sourceOptions ? options.sourceOptions : {};
+      return {
+        source: new ol.source.OSM(sourceOptions)
+      };
+    }
 };
