b561b97debef57c17aaf48507e10e2096c3c588a
Gregg Roemhildt <roemhildtg@gmail.com>
2016-07-28 14:13:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import 'identify-widget/';
-import 'layer-control/';
-import 'locator-widget/';
-import 'measure-widget/';
-import 'ol-map/';
-import 'ol-popup/';
-import 'print-widget/';
+import 'can-geo/identify-widget/identify-widget';
+import 'can-geo/layer-control/layer-control';
+import 'can-geo/locator-widget/locator-widget';
+import 'can-geo/measure-widget/measure-widget';
+import 'can-geo/ol-map/ol-map';
+import 'can-geo/ol-popup/ol-popup';
+import 'can-geo/print-widget/print-widget';
