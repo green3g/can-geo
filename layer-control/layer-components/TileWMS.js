515595b802a703194bc25bd230e049b66e1f8c06
Gregg Roemhildt <roemhildtg@gmail.com>
2017-05-14 16:59:22 -0500

    ENH: modular tile-wms layer

--- layer-control/layer-components/TileWMS.js
+++ layer-control/layer-components/TileWMS.js
@@ -2,9 +2,53 @@ import Component from 'can-component';
 import baseViewModel from './Default';
 import template from './TileWMS.stache!';
 import DefineList from 'can-define/list/list';
+import DefineMap from 'can-define/map/map';
 import './TileWMS.less';
 
 import dev from 'can-util/js/dev/dev';
+
+const SubLayerMap = DefineMap.extend({
+    layer: '*',
+    title: {
+        type: 'string',
+        get (title) {
+            if (!title) {
+                return this.id;
+            }
+            return title;
+        }
+    },
+    id: 'string',
+    visible: 'boolean',
+    collapsed: {
+        type: 'boolean',
+        value: true
+    },
+    baseUrl: 'string',
+    legendGraphicURL: {
+        get () {
+            let separator;
+            if (this.baseUrl.indexOf('?') > -1) {
+                separator = '';
+            } else {
+                separator = '?';
+            }
+            return [
+                this.baseUrl,
+                separator,
+                'REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&WIDTH=40&LAYER=',
+                this.id
+            ].join('');
+        }
+    },
+    toggleCollapsed: function (sub) {
+        sub.collapsed = !sub.collapsed;
+    }
+});
+
+export const SubLayerList = DefineList.extend({
+    '#': SubLayerMap
+});
 
 const ViewModel = baseViewModel.extend({
     collapsed: {
@@ -16,42 +60,24 @@ const ViewModel = baseViewModel.extend({
         value: true
     },
     sublayers: {
-        Value: DefineList
+        Value: SubLayerList,
+        Type: SubLayerList
     },
     init: function () {
         baseViewModel.prototype.init.apply(this, arguments);
         const source = this.layer.layer.getSource();
         const params = source.getParams();
-        this.sublayers = (params.LAYERS || params.layers).split(',');
-        const sublayerObjects = [];
+        const sublayers = (params.LAYERS || params.layers).split(',');
         const defaultVisible = this.defaultSublayerVisible;
-        this.sublayers.forEach((sublayer) => {
-            sublayerObjects.push({
-                title: sublayer, //TODO: make configureable title options
+        this.sublayers = sublayers.map((sublayer) => {
+            return {
+                //title: TODO get a configureable title
                 id: sublayer,
                 visible: defaultVisible,
-                collapsed: true,
-                baseUrl: source.getUrls()[0],
-                toggleCollapsed: function (sub) {
-                    sub.collapsed = !sub.collapsed;
-                },
-                getLegendGraphicURL () {
-                    let separator;
-                    if (this.baseUrl.indexOf('?') > -1) {
-                        separator = '';
-                    } else {
-                        separator = '?';
-                    }
-                    return [
-                        this.baseUrl,
-                        separator,
-                        'REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&WIDTH=40&LAYER=',
-                        this.id
-                    ].join('');
-                }
-            });
+                baseUrl: source.getUrls()[0]
+            };
         });
-        this.sublayers = sublayerObjects;
+
         this.updateSublayerVisibility();
     },
     toggleCollapsed: function () {
